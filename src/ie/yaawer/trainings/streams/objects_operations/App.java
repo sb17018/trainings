a848aec7a9f3b2b304cb07455c1b2c9c4d873d31
piotr.bar <piotr.bar@accenture.com>
2025-04-23 07:06:45 +0100

    Use many different methods on stream to manipulate data coming from "Employee"s collection

--- src/ie/yaawer/trainings/streams/objects_operations/App.java
+++ src/ie/yaawer/trainings/streams/objects_operations/App.java
@@ -2,7 +2,12 @@ package ie.yaawer.trainings.streams.objects_operations;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -10,11 +15,11 @@ public class App {
 		
 		List<Employee> empList = new ArrayList<>();
 
-		Employee emp_1 = new Employee("Peter", "HR", 45000, "Male", LocalDate.of(2000, 5, 5));
+		Employee emp_1 = new Employee("Peter", "HR", 60000, "Male", LocalDate.of(2000, 5, 5));
 		Employee emp_2 = new Employee("Chloe", "Accounting", 45000, "Female", LocalDate.of(2002, 2, 22));
 		Employee emp_3 = new Employee("Andy", "IT", 50000, "Male", LocalDate.of(2003, 7, 1));
-		Employee emp_4 = new Employee("Suzanne", "IT", 60000, "Female", LocalDate.of(2000, 9, 27));
-		Employee emp_5 = emp_2;
+		Employee emp_4 = new Employee("Suzanne", "IT", 60000, "Female", LocalDate.of(2000, 4, 27));
+		Employee emp_5 = new Employee("George", "HR", 57000, "Female", LocalDate.of(2000, 9, 27));
 		
 		empList.add(emp_1);
 		empList.add(emp_2);
@@ -35,6 +40,57 @@ public class App {
 		// select values as unique
 		List<String> depts = empList.stream().map(e -> e.getDepartment()).distinct().toList();
 		System.out.println(depts);
+		
+		// select values as unique
+		Employee highestPaid = empList.stream().max(Comparator.comparingDouble(Employee::getSalary)).get();
+		System.out.println(highestPaid.toString());
+		
+		Employee lowestPaid = empList.stream().min(Comparator.comparingDouble(Employee::getSalary)).get();
+		System.out.println(lowestPaid.toString());
+		
+		List<Employee> emplsAfter2000 = empList.stream().filter(e->e.getJoiningDate().isAfter(LocalDate.of(2000,12,31))).toList();
+		
+		List<Employee> highestPaidEmpls = new ArrayList<>();
+		
+		double highestSalary = 0.0;
+		for(Employee empl : empList) {
+			if(empl.getSalary() > highestSalary) {
+				highestPaidEmpls.clear();
+				highestSalary = empl.getSalary();
+			}
+			else if(empl.getSalary() < highestSalary) continue;
+			highestPaidEmpls.add(empl);
+		}
+		
+		highestPaidEmpls.forEach(e->System.out.println(e.getName() + ": " + e.getSalary()));
+		emplsAfter2000.forEach(System.out::println);
+		
+//		List<String> lowestPaid = empList.stream().max(Comparator::getSalary);
+//		System.out.println(lowestPaid);
+		
+		// grouping by variable
+		Map<String, Long> emplsInDepts = empList.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+		System.out.println(emplsInDepts);
+		
+		// average in groups
+		Map<String, Double> avarageInDepts = empList.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+		System.out.println(avarageInDepts);
+		
+		// senior in the organisation
+		Employee emplSenior = empList.stream().min(Comparator.comparing(Employee::getJoiningDate)).get();
+		System.out.println(emplSenior);
+		
+		// count by gender in IT
+		Map<String, Long> emplsByGender = empList.stream().filter(empl -> empl.getDepartment().equals("IT")).collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+		System.out.println(emplsByGender);
+		
+		Map<String, Map<String, Long>> emplsByGenderTwoGroupings = empList.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.groupingBy(Employee::getGender, Collectors.counting())));
+		System.out.println(emplsByGenderTwoGroupings.get("IT"));
+		
+		
+		Logger logger = Logger.getLogger("Status Logger");
+		
+		logger.log(Level.INFO, "OK");
 
 	}
 
